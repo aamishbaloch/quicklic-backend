9bb57cef77e46132eae3aa04a5d5c3510e691b09
Aamish Baloch <aamish.baloch@gmail.com>
2017-09-24 15:50:42 +0500

    Seeds added and Enums updated to Integers

--- scripts/management/commands/seeds.py
+++ scripts/management/commands/seeds.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.core.management import BaseCommand
 
+from entities.clinic.models import Country, City
+
 User = get_user_model()
 
 
@@ -12,5 +14,23 @@ class Command(BaseCommand):
     help = "Add mandatory data."
 
     def handle(self, *args, **options):
+        country_data = [
+            {
+                "name": "Pakistan",
+                "city_data": [
+                    {
+                        "name": "Lahore",
+                    },
+                    {
+                        "name": "Islamabad",
+                    }
+                ]
+            }
+        ]
+
+        for country_detail in country_data:
+            Country.objects.create(name=country_detail["name"])
+            for city_detail in country_detail["city_data"]:
+                City.objects.create(name=city_detail["name"])
 
         self.stdout.write("Task Successful")
