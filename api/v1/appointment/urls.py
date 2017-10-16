51e0f1e3a160ca2bee2b38516fdb7d0415f0ff48
Aamish Baloch <aamish.baloch@gmail.com>
2017-10-16 18:11:49 +0500

    appointment list

--- api/v1/appointment/urls.py
+++ api/v1/appointment/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.AppointmentView.as_view()),
+    url(r'^list$', views.AppointmentListView.as_view()),
 ]
--- libs/utils.py
+++ libs/utils.py
@@ -1,4 +1,5 @@
 from random import randint
+from datetime import datetime
 
 
 def str2bool(value):
@@ -13,3 +14,8 @@ def str2bool(value):
 
 def get_verification_code():
     return randint(100000, 999999)
+
+
+def get_datetime_from_date_string(datetime_str):
+    datetime_str = "{} {}".format(datetime_str, "00:00:00")
+    return datetime.strptime(datetime_str, "%Y-%m-%d %H:%M:%S")
