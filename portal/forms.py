c7f18765d3b8a136b4cad738e71410178d5a71aa
Aamish Baloch <aamish.baloch@gmail.com>
2017-10-31 17:27:56 +0500

    login and logout added in portal

--- portal/forms.py
+++ portal/forms.py
@@ -1,10 +1,14 @@
-from django.forms.models import ModelForm
-from entities.contact.models import Contact
+from django import forms
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
-class ContactForm(ModelForm):
-    class Meta:
-        model = Contact
-        fields = ['name', 'phone', 'email', 'subject', 'message']
+class LoginForm(forms.Form):
+    phone = forms.CharField(max_length=50)
+    password = forms.CharField(max_length=50)
+
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
 
 
